b5053624293ea6b9a7f6d9027e1950bca5de1a3e
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2020-07-27 22:17:38 -0300

    Styles: Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,61 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Form, Repositories, Title } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/9787691?s=460&u=c5c51a7f6c190fecd310b09adc8aa11f5d5a115b&v=4"
+            alt="Paulo de Oliveira"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/9787691?s=460&u=c5c51a7f6c190fecd310b09adc8aa11f5d5a115b&v=4"
+            alt="Paulo de Oliveira"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/9787691?s=460&u=c5c51a7f6c190fecd310b09adc8aa11f5d5a115b&v=4"
+            alt="Paulo de Oliveira"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
